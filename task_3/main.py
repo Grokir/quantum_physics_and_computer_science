5433283dca2e8a15473a650837c8662985f7c99c
Grokir <grokir13@yandex.ru>
2026-03-10 18:41:24 +0300

    End of lab work No.3: it works

--- task_3/main.py
+++ task_3/main.py
@@ -1,8 +1,6 @@
-import numpy as np
-import qutip as qt
-from qutip  import Qobj
-from numpy  import ndarray
-from math   import sqrt
+import  numpy   as np
+from    numpy   import ndarray
+from    math    import sqrt
 
 
 # Оператор NOT
@@ -48,107 +46,168 @@ def np_H(vec: ndarray = np.array([])) -> ndarray:
     return h@vec
 
 
-###########################################
+def Kronecker(lhs_matr: ndarray, rhs_matr: ndarray) -> ndarray:
+    """Тензорное произведение (произведение Кронекера)"""
+    return np.kron(lhs_matr, rhs_matr)
 
-# Оператор NOT
-def qt_X(vec: Qobj = Qobj(), square: bool = False) -> Qobj:
 
-    x: Qobj = qt.sigmax()
-
-    if square:
-        return x*x
+def Test1() -> bool:
+    # Из-за того, что в теории к Лабораторной работе №2 
+    # часть первой координаты на рисунке срезано, то
+    # пришлось округлять до двух знаков
     
-    return x*vec
+    np_ref:      ndarray = np.array([[-0.60], [0.80]])
+    np_base_ket: ndarray = np.array([[0.989],[0.145]])
+    np_res = np_X(np_Z(np_H(np_base_ket)))
+    npf = (round(np_res[0][0], 2) == np_ref[0][0]) and (round(np_res[1][0], 2) == np_ref[1][0])
 
-# Оператор Z
-def qt_Z(vec: Qobj = Qobj(), square: bool = False) -> Qobj:
-    z: Qobj = qt.sigmaz()
+    return npf
 
-    if square:
-        return z*z
-    
-    return z*vec
 
-# Оператор Адамара (H)
-def qt_H(vec: Qobj = Qobj(), square: bool = False) -> Qobj:
-    h: Qobj = Qobj(
+def Test2() -> bool:
+    # Из-за того, что в теории к Лабораторной работе №2 
+    # часть первой координаты на рисунке срезано, то
+    # пришлось округлять до двух знаков
+    x: ndarray = np.array(
+        [
+            [0.0, 1.0], 
+            [1.0, 0.0]
+        ]
+    )
+
+    z: ndarray = np.array(
+        [
+            [1.0,  0.0], 
+            [0.0, -1.0]
+        ]
+    )
+
+    h: ndarray = np.array(
         [
             [1.0,  1.0], 
             [1.0, -1.0]
         ]
     )
     h *= pow(base=sqrt(2), exp=-1)
-    if square:
-        return h*h
-    
-    return h*vec
 
+    U = x@z@h
 
-def Test() -> tuple[bool, bool]:
-    # Из-за того, что в теории к Лабораторной работе №2 
-    # часть первой координаты на рисунке срезано, то
-    # пришлось округлять до двух знаков
-    
-    np_ref: ndarray = np.array([[-0.60], [0.80]])
-    qt_ref: Qobj    = Qobj([[-0.60], [0.80]])
-
-    np_base_ket:    ndarray = np.array([[0.989],[0.145]])
-    qt_base_ket:    Qobj    = Qobj([[0.989],[0.145]]) # ket-vector{0, 1}
-
-    np_res = np_X(np_Z(np_H(np_base_ket)))
-    qt_res = qt_X(qt_Z(qt_H(qt_base_ket)))
-
-    qtf = (round(float(qt_res[0][0]), 2) == qt_ref[0][0]) and (round(float(qt_res[1][0]), 2) == qt_ref[1][0])
+    np_ref:      ndarray = np.array([[-0.60], [0.80]])
+    np_base_ket: ndarray = np.array([[0.989],[0.145]])
+    # np_res = np_X(np_Z(np_H(np_base_ket)))
+    np_res = U@np_base_ket
     npf = (round(np_res[0][0], 2) == np_ref[0][0]) and (round(np_res[1][0], 2) == np_ref[1][0])
 
-    return npf, qtf
+    return npf
 
-    
+
 
 
 def main():
 
-    # res: tuple[bool, bool] = Test()
-    # print(f"\n\nNP res: {res[0]} | QT res: {res[1]}\n\n")
+    # res: bool = Test1()
+    # print(f"\n\nNP TEST №1: round(XZH[[0.989],[0.145]])  == [[-0.60], [0.80]] : {res}\n\n")
+    # res: bool = Test2()
+    # print(f"\n\nNP TEST №2: round(XZH*[[0.989],[0.145]]) == [[-0.60], [0.80]] : {res}\n\n")
     # return
 
+    # Матрица оператора NOT
+    x: ndarray = np.array(
+        [
+            [0.0, 1.0], 
+            [1.0, 0.0]
+        ]
+    )
 
-    print("Задание 2\n")
-    print("[Часть 1] : numpy")
-    np_base_ket:    ndarray = np.array([[0.],[1.]])
-    np_res1:        ndarray = np_Z(np_X(np_base_ket))
-    np_res2:        ndarray = np_H(np_X(np_base_ket))
-    np_res3:        ndarray = np_X(np_Z(np_H(np_base_ket)))
-    np_res4:        ndarray = np_H(np_Z(np_Z(np_X(np_base_ket))))
-    np_res5:        ndarray = np_Z(np_Z(np_H(np_H(np_base_ket))))
-    np_res6:        ndarray = np_X(np_X(np_X(np_base_ket)))
-    print(f"[0] base_ket:  \n{np_base_ket}\n")
-    print(f"[1] |ψ'> = Z*X*|ψ>:  \n{np_res1}\n")
-    print(f"[2] |ψ'> = H*X*|ψ>:\n{np_res2}\n")
-    print(f"[3] |ψ'> = X*Z*H*|ψ>:\n{np_res3}\n")
-    print(f"[4] |ψ'> = H*Z*Z*X|ψ>:\n{np_res4}\n")
-    print(f"[5] |ψ'> = Z*Z*H*H|ψ>:\n{np_res5}\n")
-    print(f"[6] |ψ'> = X*X*X|ψ>:\n{np_res6}\n")
+    # Матрица оператора Z
+    z: ndarray = np.array(
+        [
+            [1.0,  0.0], 
+            [0.0, -1.0]
+        ]
+    )
 
+    # Матрица оператора H
+    h: ndarray = np.array(
+        [
+            [1.0,  1.0], 
+            [1.0, -1.0]
+        ]
+    )
+    h *= pow(base=sqrt(2), exp=-1)
+
+
+    print("Задание 3")
+    print("Часть 1: U1 = U2 = U")
+    
+    bz_ket_0: ndarray = np.array([[1.],[0.]])
+    bz_ket_1: ndarray = np.array([[0.],[1.]])
+
+    bz_ket_00: ndarray = Kronecker(bz_ket_0, bz_ket_0)
+    bz_ket_01: ndarray = Kronecker(bz_ket_0, bz_ket_1)
+    bz_ket_10: ndarray = Kronecker(bz_ket_1, bz_ket_0)
+    bz_ket_11: ndarray = Kronecker(bz_ket_1, bz_ket_1)
+
+
+    print("\n1. XZH = U")
+
+
+    # Способ 1-й: "Влоб" или "почленно"
+    # xzh_0: ndarray = np_X(np_Z(np_H(bz_ket_0)))
+    # xzh_1: ndarray = np_X(np_Z(np_H(bz_ket_1)))
+    # print(f"   XZH|00> :\n{Kronecker(xzh_0, xzh_0)}\n")
+    # print(f"   XZH|01> :\n{Kronecker(xzh_0, xzh_1)}\n")
+    # print(f"   XZH|10> :\n{Kronecker(xzh_1, xzh_0)}\n")
+    # print(f"   XZH|11> :\n{Kronecker(xzh_1, xzh_1)}\n")
+
+    # Способ 2-й: через матричное произв-ие Кронекера 
+    U1 = x@z@h
+    print(f"   (U1 ⊗ U2)|00> :\n{Kronecker(U1, U1)@bz_ket_00}\n")
+    print(f"   (U1 ⊗ U2)|01> :\n{Kronecker(U1, U1)@bz_ket_01}\n")
+    print(f"   (U1 ⊗ U2)|10> :\n{Kronecker(U1, U1)@bz_ket_10}\n")
+    print(f"   (U1 ⊗ U2)|11> :\n{Kronecker(U1, U1)@bz_ket_11}\n")
+    
     # добавим разделитель
-    print(("=" * 70 + '\n'))
+    print(("-" * 35 + '\n'))
 
-    print("[Часть 2] : qutip")
-    qt_base_ket:    Qobj = qt.ket('1') # ket-vector{0, 1}
-    qt_res1:        Qobj = qt_Z(qt_X(qt_base_ket))
-    qt_res2:        Qobj = qt_H(qt_X(qt_base_ket))
-    qt_res3:        Qobj = qt_X(qt_Z(qt_H(qt_base_ket)))
-    qt_res4:        Qobj = qt_H(qt_Z(qt_Z(qt_X(qt_base_ket))))
-    qt_res5:        Qobj = qt_Z(qt_Z(qt_H(qt_H(qt_base_ket))))
-    qt_res6:        Qobj = qt_X(qt_X(qt_X(qt_base_ket)))
-    print(f"[0] base_ket:  \n{qt_base_ket}\n")
-    print(f"[1] |ψ'> = Z*X*|ψ>:  \n{qt_res1}\n")
-    print(f"[2] |ψ'> = H*X*|ψ>:\n{qt_res2}\n")
-    print(f"[3] |ψ'> = X*Z*H*|ψ>:\n{qt_res3}\n")
-    print(f"[4] |ψ'> = H*Z*Z*X|ψ>:\n{qt_res4}\n")
-    print(f"[5] |ψ'> = Z*Z*H*H|ψ>:\n{qt_res5}\n")
-    print(f"[6] |ψ'> = X*X*X|ψ>:\n{qt_res6}\n")
 
+    print("\n2. HXZ = U")
+
+    # Способ 1-й: "Влоб" или "почленно"
+    # hxz_0: ndarray = np_H(np_X(np_Z(bz_ket_0)))
+    # hxz_1: ndarray = np_H(np_X(np_Z(bz_ket_1)))
+    # print(f"   HXZ|00> :\n{Kronecker(hxz_0, hxz_0)}\n")
+    # print(f"   HXZ|01> :\n{Kronecker(hxz_0, hxz_1)}\n")
+    # print(f"   HXZ|10> :\n{Kronecker(hxz_1, hxz_0)}\n")
+    # print(f"   HXZ|11> :\n{Kronecker(hxz_1, hxz_1)}\n")
+
+    # Способ 2-й: через матричное произв-ие Кронекера
+    U2 = h@x@z
+    print(f"   (U1 ⊗ U2)|00> :\n{Kronecker(U2, U2)@bz_ket_00}\n")
+    print(f"   (U1 ⊗ U2)|01> :\n{Kronecker(U2, U2)@bz_ket_01}\n")
+    print(f"   (U1 ⊗ U2)|10> :\n{Kronecker(U2, U2)@bz_ket_10}\n")
+    print(f"   (U1 ⊗ U2)|11> :\n{Kronecker(U2, U2)@bz_ket_11}\n")
+
+
+    print('\n', ('<' + "#" * 35 + '>'), '\n\n')
+
+    print("Часть 2: U1 ≠ U2")
+    print("   1. XZH = U1")
+    print("   2. HXZ = U2\n")
+    print(f"   (U1 ⊗ U2)|00> :\n{Kronecker(U2, U1)@bz_ket_00}\n")
+    print(f"   (U1 ⊗ U2)|01> :\n{Kronecker(U2, U1)@bz_ket_01}\n")
+    print(f"   (U1 ⊗ U2)|10> :\n{Kronecker(U2, U1)@bz_ket_10}\n")
+    print(f"   (U1 ⊗ U2)|11> :\n{Kronecker(U2, U1)@bz_ket_11}\n")
+    
+    # добавим разделитель
+    print(("-" * 35 + '\n'))
+    
+    print("   1. XZH = U2")
+    print("   2. HXZ = U1\n")
+    print(f"   (U1 ⊗ U2)|00> :\n{Kronecker(U2, U1)@bz_ket_00}\n")
+    print(f"   (U1 ⊗ U2)|01> :\n{Kronecker(U2, U1)@bz_ket_01}\n")
+    print(f"   (U1 ⊗ U2)|10> :\n{Kronecker(U2, U1)@bz_ket_10}\n")
+    print(f"   (U1 ⊗ U2)|11> :\n{Kronecker(U2, U1)@bz_ket_11}\n")
 
 if __name__ == "__main__":
     main()
